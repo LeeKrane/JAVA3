0c8d6b95a160b2d8c09105a978d2577d2314e873
LeeKrane <godfist.leekrane@gmail.com>
2019-11-11 20:14:47 +0100

    - started labor09
    Signed-off-by: Lee Krane <godfist.leekrane@gmail.com>

---
+++ tests/labors/labor09/SchuelerVerwaltungTest.java
@@ -0,0 +1,172 @@
+package labors.labor09;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author maus, update scre
+ */
+public class SchuelerVerwaltungTest {
+
+    private SchuelerVerwaltung informatik;
+    private Schueler andrae, riegler, ines;
+
+    public SchuelerVerwaltungTest() {
+        try {
+            informatik = new SchuelerVerwaltung("resources/labors/labor09/Schueler_SortName.csv");
+        } catch (IOException ex) {
+            System.err.println("File not found. Should be in project root directory. ");
+        }
+    }
+
+    @Before
+    public void setUp() {
+        andrae = Schueler.makeSchueler("1AHIF;Andrae;Franz Eduardo;m;09.12.2001;röm.-kath.");
+        riegler = Schueler.makeSchueler("1AHIF;Riegler;Marvin;m;01.09.2002;evang. A.B.");
+        ines = Schueler.makeSchueler("3BHIF;Lapatschka;Ines;w;19.03.2000;evang. A.B.");
+    }
+
+    /**
+     * Test of schuelerEinerKlasse method, of class SchuelerVerwaltung.
+     */
+    @Test
+    public void testGetSchuelerFromKlase() {
+        String klasse = "1AHIF";
+        Set<Schueler> result = informatik.getSchuelerFromKlasse(klasse);
+        assertEquals(36, result.size());
+        System.out.println(result);
+        assertTrue(result.contains(andrae));
+        assertTrue(result.contains(riegler));
+        result = informatik.getSchuelerFromKlasse("3BHIF");
+        assertEquals(27, result.size());
+        assertTrue(result.contains(ines));
+    }
+
+    /**
+     * Test of containsName method, of class SchuelerVerwaltung.
+     */
+    @Test
+    public void testContainsName() {
+        String name = "Andrae";
+        Set<Schueler> result = informatik.containsName(name, true);
+        assertEquals(1, result.size());
+        assertTrue(result.contains(andrae));
+        result = informatik.containsName("Riegler", true);
+        assertEquals(3, result.size());
+        List<Schueler> liste = new ArrayList<>(result);
+        assertEquals("Lukas", liste.get(1).getVorname());
+        assertEquals("3AHIF", liste.get(2).getKlasse());
+        assertEquals("evang. A.B.", liste.get(0).getReligion());
+        result = informatik.containsName("mann", false);
+        assertEquals(6, result.size());
+        liste = new ArrayList<>(result);
+        Schueler schueler = liste.get(0);
+        assertEquals("Hofmann", schueler.getName());
+        schueler = liste.get(3);
+        assertEquals("Peter", schueler.getVorname());
+        assertEquals(LocalDate.of(1996, 1, 20), liste.get(5).getGeboren());
+    }
+
+    /**
+     * Test of maennlichWeiblich method, of class SchuelerVerwaltung.
+     */
+    @Test
+    public void testGetAllWith() {
+        Set<Schueler> result = informatik.getAllWith('w');
+        assertEquals(result.size(), 24);
+        List<Schueler> liste = new ArrayList<>(result);
+        assertEquals("Jasmin Juliane Edeltraud", liste.get(1).getVorname());
+        assertEquals(LocalDate.of(1998, 5, 24), liste.get(23).getGeboren());
+        assertEquals("3BHIF", liste.get(14).getKlasse());
+        assertEquals(ines, liste.get(14));
+    }
+
+    /**
+     * Test of klassenAnzahl method, of class SchuelerVerwaltung.
+     */
+    @Test
+    public void testGetKlassenAnzahl() {
+        Map<String, Integer> result = informatik.getKlassenAnzahl();
+        assertEquals(36, (int) result.get("1AHIF"));
+        assertEquals(19, (int) result.get("2BHIF"));
+        assertEquals(27, (int) result.get("3AHIF"));
+        assertEquals(20, (int) result.get("4CHIF"));
+        assertEquals(20, (int) result.get("5BHIF"));
+    }
+
+    /**
+     * Test of religionsZugehoerigkeit method, of class SchuelerVerwaltung.
+     */
+    @Test
+    public void testGetReligionsZugehoerigkeit() {
+        Map<String, Map<String, List<String>>> result
+                = informatik.getReligionsZugehoerigkeit();
+        assertEquals(2, result.get("ALEVI").size());
+        assertNull(result.get("evang. A. B."));
+        assertEquals(8, result.get("evang. A.B.").size());
+        assertTrue(result.get("islam.").keySet().contains("5BHIF"));
+        Map<String, List<String>> religionsListe = result.get("islam.");
+        assertTrue(religionsListe.get("2BHIF").contains("Harbas"));
+        assertEquals(1, religionsListe.get("2BHIF").size());
+    }
+
+    /**
+     * Test of geborenBis method, of class SchuelerVerwaltung.
+     */
+    /*
+    @Test
+    public void testGetGeborenBis() {
+        LocalDate heute = LocalDate.now();
+        Set<Schueler> result = informatik.getGeborenBis(heute, true);
+        assertEquals(324, result.size());
+        result = informatik.getGeborenBis(heute, false);
+        assertEquals(0, result.size());
+        result = informatik.getGeborenBis(LocalDate.of(1999, 7, 1), true);
+        assertEquals(120, result.size());
+        result = informatik.getGeborenBis(LocalDate.of(1999, 7, 1), false);
+        assertEquals(204, result.size());
+    }
+    
+     */
+
+    /**
+     * Test of geburtstagsListe method, of class SchuelerVerwaltung.
+     */
+    @Test
+    public void testGeburtstagsListeIntArg() {
+        Map<LocalDate, Set<String>> result = informatik.getGeburtstagsListe(2017);
+        assertEquals(213, result.size());
+        assertEquals(5, informatik.getGeburtstagsListe(1995).size());
+        assertEquals(Collections.EMPTY_MAP, informatik.getGeburtstagsListe(1901));
+        List<Set<String>> namensListe = new ArrayList<>(result.values());
+        assertTrue(namensListe.get(5).contains("Mrskos Julia Marlene 1CHIF 15"));
+        assertEquals(2, namensListe.get(5).size());
+        assertEquals(7, namensListe.get(43).size());
+    }
+
+    /**
+     * Test of geburtstagsListe method, of class SchuelerVerwaltung.
+     */
+    @Test
+    public void testGeburtstagsListeNoArgs() {
+        Map<LocalDate, Set<String>> result = informatik.getGeburtstagsListe();
+        assertEquals(213, result.size());
+    }
+
+}
