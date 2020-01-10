c61f8a91c49563d5b86787571b310a4b5914d992
LeeKrane <godfist.leekrane@gmail.com>
2020-01-10 10:46:12 +0100

    - updated src/labors/labor12/DeleteLines.java

--- src/labors/labor12/DeleteLines.java
+++ src/labors/labor12/DeleteLines.java
@@ -1,23 +1,36 @@
 package labors.labor12;
 
 import java.io.*;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author LeeKrane
  */
+
+// TODO: refactor
 
 public class DeleteLines {
 	public static void main (String[] args) {
 		if (!args[3].matches("(\\d+-\\d+)|\\d+"))
 			System.err.println("Error: wrong input.\nCommand: " + DeleteLines.class.getSimpleName() + " <input filename> <output filename> <startLine-endLine>|<endLine>");
 		else {
-			try (AsciiInputStream is = new AsciiInputStream(args[1])) {
-				if (args[3].matches("\\d+-\\d+")) {
-					String[] split = args[3].split("-");
-					export(args[2], readFromFile(is, args[1], Integer.parseInt(split[0]), Integer.parseInt(split[1])));
+			int[] excludeStarts = new int[args.length - 3];
+			int[] excludeEnds = new int[args.length - 3];
+			String[] split;
+			for (int i = 3; i < args.length; i++) {
+				if (args[i].matches("\\d+-\\d+")) {
+					split = args[i].split("-");
+					excludeStarts[i - 3] = Integer.parseInt(split[0]);
+					excludeEnds[i - 3] = Integer.parseInt(split[1]);
 				}
-				else
-					export(args[2], readFromFile(is, args[1], Integer.parseInt(args[3])));
+				else {
+					excludeStarts[i - 3] = Integer.parseInt(args[i]);
+					excludeEnds[i - 3] = Integer.parseInt(args[i]);
+				}
+			}
+			try (AsciiInputStream is = new AsciiInputStream(args[1])) {
+				export(args[2], readFromFile(is, getExcludedLines(excludeStarts, excludeEnds)));
 			} catch (IOException e) {
 				System.err.println(e.getMessage());
 			}
@@ -30,32 +43,51 @@ public class DeleteLines {
 		}
 	}
 	
-	private static String readFromFile (AsciiInputStream inputStream, String filename, int exclude) throws IOException {
+	private static String readFromFile (AsciiInputStream inputStream, Set<Integer> excludes) throws IOException {
+		if (excludes.size() == 0)
+			return new String(inputStream.readAllBytes());
 		StringBuilder builder = new StringBuilder();
-		inputStream.skipLines(exclude);
-		builder.append(inputStream.readLine());
-		while (inputStream.available() > 0) {
-			builder.append('\n').append(inputStream.readLine());
+		int lastDeleted = 0;
+		if (!excludes.contains(0)) {
+			builder.append(inputStream.readLine());
+			lastDeleted = 1;
 		}
+		for (int i = lastDeleted; i <= getMax(excludes); i++) {
+			if (inputStream.available() == 0)
+				break;
+			if (excludes.contains(i))
+				inputStream.skipLines(1);
+			else
+				builder.append(inputStream.readLine()).append('\n');
+		}
+		builder.append(new String(inputStream.readAllBytes()));
 		return builder.toString();
 	}
 	
-	private static String readFromFile (AsciiInputStream inputStream, String filename, int excludeStart, int excludeEnd) throws IOException {
-		if (excludeStart >= excludeEnd)
-			return new String(inputStream.readAllBytes());
-		StringBuilder builder = new StringBuilder();
-		if (excludeStart > 0)
-			builder.append(inputStream.readLine());
-		for (int i = 1; i < excludeStart; i++)
-			builder.append('\n').append(inputStream.readLine());
-		inputStream.skipLines(excludeEnd - excludeStart);
-		while (inputStream.available() > 0)
-			builder.append('\n').append(inputStream.readLine());
-		return builder.toString();
+	private static int getMax (Set<Integer> set) {
+		int max = 0;
+		for (Integer i : set) {
+			if (i > max)
+				max = i;
+		}
+		return max;
+	}
+	
+	private static Set<Integer> getExcludedLines (int[] excludeStarts, int[] excludeEnds) {
+		Set<Integer> excludedLines = new TreeSet<>();
+		for (int i = 0; i < excludeStarts.length; i++) {
+			for (int j = excludeStarts[i]; j <= excludeEnds[i]; j++)
+				excludedLines.add(j);
+		}
+		return excludedLines;
 	}
 }
 
 class AsciiInputStream extends FileInputStream {
+	public static void main (String[] args) {
+		DeleteLines.main(new String[]{"DeleteLines", "res/labors/labor12/input.txt", "res/labors/labor12/output.txt", "1-2", "4", "5", "8"});
+	}
+	
 	public AsciiInputStream (String name) throws FileNotFoundException {
 		super(name);
 	}
