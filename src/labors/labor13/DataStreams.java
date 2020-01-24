64b08ba6b6ec81f11ea9ffa83df29fad73f665a3
LeeKrane <godfist.leekrane@gmail.com>
2020-01-24 11:18:07 +0100

    - solved src/labors/labor13/DataStreams.java

---
+++ src/labors/labor13/DataStreams.java
@@ -0,0 +1,121 @@
+package labors.labor13;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+public class DataStreams {
+	public static void main (String[] args) {
+		try {
+			convertToBinary("res/labors/labor13/input.txt", "res/labors/labor13/output.bin");
+			Queue<Character> types = new LinkedList<>();
+			Map<String, Queue<Object>> data = readBinary("res/labors/labor13/output.bin", types);
+			reconstructFile("res/labors/labor13/output.txt", data, types);
+		} catch (IOException e) {
+			System.err.println(e + ": " + e.getMessage());
+		}
+	}
+	
+	private static void convertToBinary (String inputFilename, String outputFilename) throws IOException {
+		try (BufferedReader reader = new BufferedReader(new FileReader(inputFilename));
+			 DataOutputStream os = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(outputFilename)))) {
+			while (reader.ready()) {
+				String line = reader.readLine();
+				if (writeInteger(line, os)) continue;
+				if (writeDouble(line, os)) continue;
+				writeBoolean(line, os);
+			}
+		}
+	}
+	
+	private static void writeBoolean (String line, DataOutputStream os) throws IOException {
+		switch (line) {
+			case "true":
+				os.writeChar('t');
+				break;
+			case "false":
+				os.writeChar('f');
+				break;
+			default:
+				throw new IllegalStateException();
+		}
+	}
+	
+	private static boolean writeDouble (String line, DataOutputStream os) throws IOException {
+		try {
+			double input = Double.parseDouble(line);
+			os.writeChar('d');
+			os.writeDouble(input);
+			return true;
+		} catch (NumberFormatException ignored) {}
+		return false;
+	}
+	
+	private static boolean writeInteger (String line, DataOutputStream os) throws IOException {
+		try {
+			int input = Integer.parseInt(line);
+			os.writeChar('i');
+			os.writeInt(input);
+			return true;
+		} catch (NumberFormatException ignored) {}
+		return false;
+	}
+	
+	private static Map<String, Queue<Object>> readBinary (String filename, Queue<Character> types) throws IOException {
+		try (DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)))) {
+			Map<String, Queue<Object>> ret = new HashMap<>();
+			Queue<Object> integers = new LinkedList<>();
+			Queue<Object> doubles = new LinkedList<>();
+			Queue<Object> booleans = new LinkedList<>();
+			char ch;
+			while (dis.available() > 0) {
+				ch = dis.readChar();
+				switch (ch) {
+					case 'i':
+						integers.add(dis.readInt());
+						types.add('i');
+						break;
+					case 'd':
+						doubles.add(dis.readDouble());
+						types.add('d');
+						break;
+					case 't':
+					case 'f':
+						booleans.add(ch == 't');
+						types.add(ch);
+						break;
+					default:
+						throw new IllegalStateException();
+				}
+			}
+			ret.put("Integer", integers);
+			ret.put("Double", doubles);
+			ret.put("Boolean", booleans);
+			return ret;
+		}
+	}
+	
+	private static void reconstructFile (String filename, Map<String, Queue<Object>> data, Queue<Character> types) throws IOException {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+			while (!types.isEmpty()) {
+				switch (types.poll()) {
+					case 'i':
+						writer.write(data.get("Integer").poll().toString());
+						break;
+					case 'd':
+						writer.write(data.get("Double").poll().toString());
+						break;
+					case 't':
+					case 'f':
+						writer.write(data.get("Boolean").poll().toString());
+						break;
+					default:
+						throw new IllegalStateException();
+				}
+				writer.newLine();
+			}
+		}
+	}
+}
